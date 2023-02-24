a32a33666226122cf50ae7cd3dc71c99fe83167b
mani5140 <srimanikantagarapati12@gmail.com>
2023-02-24 15:18:23 +0530

    added

---
+++ src/footer.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div>
+      <footer>
+        <h4>
+         By Team Flash
+      
+        </h4>
+      </footer>
+    </div>
+  );
+};
+
+export default Footer;
